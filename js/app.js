e95992772e712bb65255939ceb2efc61ed0020c9
ganesh-raja-au6 <ganeshrajamaddi@gamil.com>
2020-04-11 14:09:56 +0530

    hi

--- js/app.js
+++ js/app.js
@@ -41,10 +41,10 @@ let businesslist = (e) => {
             <div class="col-xl-2 col-lg-2 text-right col-md-4 col-sm-12 col-6">
                 <div class="user-vendor">
                     <div class="dropdown">
-                        <a class="dropdown-toggle" id="dropdownMenuButton" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="user-icon">  <img src="images/dashboard-profile.jpg" alt="" class="rounded-circle mb10"></span><span class="user-vendor-name">${
                           JSON.parse(user).username
-                        }</span> 
+                        }</span>
                     </a>
                         <div class=" dashboard-dropdown-menu dropdown-menu" aria-labelledby="dropdownMenuButton">
                         <a class="dropdown-item" onclick='return vendorDashboard()'>Dashboard </a>
